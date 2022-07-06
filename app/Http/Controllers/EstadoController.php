f9dadcdc6f60c70f34d0979541ea8378fb4ac44f
Gerardo Ramirez <sramirezse@gmail.com>
2022-07-05 20:36:58 -0500

    Primeros cambios

--- app/Http/Controllers/EstadoController.php
+++ app/Http/Controllers/EstadoController.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\GetStateRequest;
 use App\Models\Estado;
 use App\Repositories\EstateRepository;
 use Illuminate\Http\Request;
 
 class EstadoController extends Controller
 {
-    public function getState($cp, EstateRepository $estateRepository )
+    public function getState($cp, EstateRepository $estateRepository, Request $request)
     {
-        $estates = $estateRepository->getState($cp);
-        return response()->json($estates);
+            $estates = $estateRepository->getState($cp);
+            return response()->json($estates);
     }
 }
